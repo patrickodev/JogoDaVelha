61a29399d87d5c44263d9f9d89ed20c57d041bd8
patrickodev <50785856+patrickodev@users.noreply.github.com>
2019-10-31 20:10:18 -0300

    Code

---
+++ JogoDaVelha/src/jogodavelha/Jogo.java
@@ -0,0 +1,93 @@
+package jogodavelha;
+
+import java.util.Scanner;
+
+public class Jogo {
+    private final Tabuleiro tabuleiro;
+    private int rodada=1, vez=1;
+    private Jogador jogador1;
+    private Jogador jogador2;
+    public Scanner entrada = new Scanner(System.in);
+
+    
+    @SuppressWarnings("empty-statement")
+    public Jogo(){
+        tabuleiro = new Tabuleiro();
+        iniciarJogadores();
+        
+        while( Jogar() );
+    }
+    
+    private void iniciarJogadores(){
+        this.jogador1 = new Humano(1) {
+            
+        };
+        
+        this.jogador2 = new Humano(2) {
+            }; 
+        
+        
+    }
+      
+    private boolean Jogar(){
+        if(ganhou() == 0 ){
+            System.out.println("----------------------");
+            System.out.println("\nRodada "+rodada);
+            System.out.println("É a vez do jogador " + vez() );
+            
+            if(vez()==1)
+                jogador1.jogar(tabuleiro);
+            else
+                jogador2.jogar(tabuleiro);
+            
+            
+            if(tabuleiro.tabuleiroCompleto()){
+                System.out.println("Tabuleiro Completo. Jogo empatado");
+                return false;
+            }
+
+            vez++;
+            rodada++;
+
+
+            return true;
+
+        } else{
+            if(ganhou() == -1 )
+                System.out.println("Jogador 1 ganhou!");
+            else
+                System.out.println("Jogador 2 ganhou!");
+            
+            return false;
+        }
+            
+    }
+    
+    public int vez(){
+        if(vez%2 == 1)
+            return 1;
+        else
+            return 2;
+    }
+    
+    public int ganhou(){
+        if(tabuleiro.checaLinhas() == 1)
+            return 1;
+        if(tabuleiro.checaColunas() == 1)
+            return 1;
+        if(tabuleiro.checaDiagonais() == 1)
+            return 1;
+        
+        if(tabuleiro.checaLinhas() == -1)
+            return -1;
+        if(tabuleiro.checaColunas() == -1)
+            return -1;
+        if(tabuleiro.checaDiagonais() == -1)
+            return -1;
+        
+        return 0;
+    }
+    
+    
+}
+
---
+++ JogoDaVelha/src/jogodavelha/JogoDaVelha.java
@@ -0,0 +1,11 @@
+package jogodavelha;
+
+
+public class JogoDaVelha {
+
+    
+    public static void main(String[] args) {
+        Jogo jogo = new Jogo();
+    }
+    
+}
